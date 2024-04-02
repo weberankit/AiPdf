6abbe39dec8d2ec93ea6edf8762d4d6c19c4a47b
ANKIT KUMAR <codingank@gmail.com>
2024-04-02 08:38:29 +0530

    utils files added

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyADqvuE0vS55AbgnNu-A7TX51WfMXS1nfw",
+  authDomain: "aipdf-375e4.firebaseapp.com",
+  projectId: "aipdf-375e4",
+  storageBucket: "aipdf-375e4.appspot.com",
+  messagingSenderId: "446862940565",
+  appId: "1:446862940565:web:8c8d4b14421fbf2481fe9c",
+  measurementId: "G-JKTX7L36JX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
