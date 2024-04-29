484ed808bd4ee9058de9ca0400c480692109ae3f
ANKIT KUMAR <codingank@gmail.com>
2024-04-29 15:35:37 +0530

    automatic scroll features bug solve

--- src/components/GeminiResponse.js
+++ src/components/GeminiResponse.js
@@ -26,10 +26,10 @@ const element=document.getElementById(id)
 element.scrollIntoView({behavior:"smooth"})
 }
 
-let countResponse=selectgeminiResponse.length
+
 useEffect(()=>{
 scrollToElement("lastOne")
-},[countResponse])
+},[selectgeminiResponse?.length])
 
 
 
@@ -44,13 +44,14 @@ return(
         
         )
     })}
+    <div id="lastOne">
     {selectgeminiResponse.length &&
-     <div className="relative" id="lastOne">
+     <div className="relative" >
           <div className="parent cursor-pointer">to know more  <p className=" text-sm element absolute top-0 bg-gray-700 ">type your question and for more better result select paragraph in pdf for context</p></div>
     <textarea className="text-black " required placeholder="to ask type here and ask" value={userInput} onChange={(e)=>setuser(e.target.value)} ></textarea>
     <button onClick={()=> handleClick()}>save</button>
 </div>
-}
+}</div>
     </>
 )
 }
