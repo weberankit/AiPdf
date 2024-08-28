76d7b4e7f845d7d3252266fa3718f76dfb32e3af
weberankit <codingank@gmail.com>
2024-08-28 09:29:15 +0530

    instrucetion chnaged

--- src/utils/tourSteps.js
+++ src/utils/tourSteps.js
@@ -2,10 +2,8 @@
 const tourSteps = [
     {
       target: '.step-1',
-      content: ` 👲--you have to signIn first as after signIn you will able to upload and see  all navigation links .
-                 👲--after login,you can upload files i.e pdf/text files or you can check demo .
-                 👲-- you will get two options to open file .
-                 👲 -- just select text in pdf and 3-option will be visible i.e AI,D,T click on any and use it happily.
+      content: ` 👲--Hi this is Aipdf . you can click  the DEMO button for Demo before this.
+                    check how to use video/Gif below.
       
       
       `,
